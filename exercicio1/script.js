7ea292cb9e527385c3c5438da9c25620f76281e8
Raissa Baeta <rbaetadev@gmail.com>
2022-08-29 16:37:33 -0300

    Exercício1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+const nome = prompt("Qual seu nome?");
+let idade = Number(prompt("Qual é a sua idade?"));
+
+console.log(typeof nome, typeof idade)
+console.log(`Meu nome é ${nome}, eu tenho ${idade} anos.`)
---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+let primeiraPergunta = "Você almoçou hoje?";
+let primeiraResposta = prompt(primeiraPergunta);
+
+let segundaPergunta = "Você fez o exercício?"
+let segundaResposta = prompt(segundaPergunta);
+
+let terceiraPergunta = "Você está com dúvidas?"
+let terceiraResposta = prompt(terceiraPergunta);
+
+console.log(primeiraPergunta, primeiraResposta);
+console.log(segundaPergunta, segundaResposta);
+console.log(terceiraPergunta, terceiraResposta)
